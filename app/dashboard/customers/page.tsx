28562f5f2b669666500cf6ddfaf747f8f890b61e
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-10-28 19:39:35 +0000

    fix search on customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,22 +1,21 @@
 import Table from '@/app/ui/customers/table';
 import { Metadata } from 'next';
-import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default async function Page() {
-    const customerNames = (await fetchCustomers()).map(obj => obj.name);
-    const customers = (await Promise.all(
-        customerNames.map(
-            name => fetchFilteredCustomers(name)
-        )
-    )).flat();
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
 
     return (
         <>
-            <Table customers={customers} />
+            <Table query={query} />
         </>
     );
 }
